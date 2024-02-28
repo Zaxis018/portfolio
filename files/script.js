e12bef9446997b33ad68fd7688288cbbc7034bae
kshitiz <106225986+Zaxis018@users.noreply.github.com>
2024-02-28 18:25:31 +0545

    Add files via upload

---
+++ files/script.js
@@ -0,0 +1,19 @@
+
+var typed = new Typed(".text", {
+    strings: ["Computer Engineer","Web Developer","Machine Learning Enthusiast" ,],
+    typeSpeed:50,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
+
+
+const toTop = document.querySelector(".top");
+window.addEventListener("scroll",() =>{
+    if (window.pageYOffset > 100){
+        toTop.classList.add("active");
+    }
+    else{
+        toTop.classList.remove("active");
+    }
+})
